cec9d0d978d4a5f5ffcd19fa9fb86bc659339484
vLorente <valenlj7@gmail.com>
2024-05-28 18:09:22 +0200

    Delete unnecesary comment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-// TODO Add module configuration into Astro Integrations
-
 export { default as CookieGuardBanner } from './components/CookieGuardBanner/CookieGuardBanner'
 export * from './components/CookieGuardBanner/CookieGuardBanner'
 export * from './components/CookieGuardBanner/CookieGuardBanner.css'
